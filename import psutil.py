e0bc4298db1213a5d07abaae750f674840f82589
Kay <120117303+illicitaffairs@users.noreply.github.com>
2023-07-01 13:05:41 -0300

    Add files via upload

---
+++ import psutil.py
@@ -0,0 +1,12 @@
+import psutil
+
+def monitorar_temperatura():
+    while True:
+        temperatura = psutil.sensors_temperatures()['coretemp'][0].current
+        if temperatura > 30:
+            print(f"A temperatura do computador é {temperatura}°C. Cuidado!")
+            # Aqui você pode adicionar a lógica para enviar um aviso, como um e-mail ou uma notificação.
+        else:
+            print(f"A temperatura do computador é {temperatura}°C.")
+
+monitorar_temperatura()
