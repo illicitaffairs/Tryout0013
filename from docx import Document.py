95911288891355c41c440b5caefa97359b9e4e99
Kay <120117303+illicitaffairs@users.noreply.github.com>
2023-07-01 13:15:19 -0300

    Add files via upload

---
+++ from docx import Document.py
@@ -0,0 +1,10 @@
+from docx import Document
+
+def salvar_no_word(texto):
+    document = Document()
+    document.add_paragraph(texto)
+    document.save('texto_no_word.docx')
+    print("Texto salvo no Word com sucesso!")
+
+texto_digitado = input("Digite o texto que deseja salvar no Word: ")
+salvar_no_word(texto_digitado)
